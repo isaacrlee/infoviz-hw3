a536355e78bca7ace463fd40727c7e5b19dbc0f1
isaacrlee <isaacrlee@gmail.com>
2019-05-08 19:36:13 -0500

    Refactor script

--- script.js
+++ script.js
@@ -1,76 +1,24 @@
-WIDTH = 480;
-HEIGHT = 480;
+const width = 480;
+const height = (width / 50) * 47;
+const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+const innerWidth = width - margin.left - margin.right;
+const innerHeight = height - margin.top - margin.bottom;
 
-// Draw Court
-var margin = { top: 20, right: 20, bottom: 20, left: 20 };
-var chartDiv = document.getElementById("court");
-var court = d3
-  .select(chartDiv)
-  .append("court")
-  .append("svg");
-court.attr("width", WIDTH).attr("height", (HEIGHT / 50) * 47);
-
-court.append("table");
-
-// var heat_g = court.append("g");
-var court_g = court.append("g");
+var court = draw_court();
+var full_data; // full data
 var shot_g = court.append("g");
-var title = d3.select(document.getElementById("caption")).append("text");
-
-const court_xScale = d3.scaleLinear().domain([-25, 25]);
-const court_yScale = d3.scaleLinear().domain([-4, 43]);
-
-const innerWidth = WIDTH - margin.left - margin.right;
-const innerHeight = HEIGHT - margin.top - margin.bottom;
 
 const shot_xScale = d3
   .scaleLinear()
   .domain([-250, 250])
   .range([margin.left, innerWidth]);
+
 const shot_yScale = d3
   .scaleLinear()
   .domain([-45, 420])
   .range([margin.top, innerHeight]);
 
-var color = d3.scaleSequential(d3.interpolateOrRd).domain([5e-6, 3e-2]); // Points per square pixel.
-
-var Basket = court_g.append("circle");
-var CornerThreeLeft = court_g.append("rect");
-var CornerThreeRight = court_g.append("rect");
-var ThreeLine = court_g.append("path");
-
-draw_court();
-
-var full_data; // full data
-
-// Shots
-d3.csv("nba_savant.csv", function(d) {
-  return {
-    name: d.name,
-    teamName: d.team_name,
-    shotMadeFlag: d.shot_made_flag,
-    actionType: d.action_type,
-    shotType: d.shot_type,
-    x: d.x,
-    y: d.y
-  };
-}).then(function(d) {
-  full_data = d;
-  renderPlot(full_data);
-});
-
-function renderPlot(data) {
-  var shots = shot_g.selectAll("circle").data(data);
-  shots.exit().remove();
-
-  shots
-    .enter()
-    .append("circle")
-    .attr("cx", d => shot_xScale(d.x))
-    .attr("cy", d => shot_yScale(d.y))
-    .attr("r", 5)
-    .attr("fill", d => (d.shotMadeFlag == 1 ? "red" : "blue"));
-}
+addShots();
 
 d3.select("#inds").on("change", function() {
   var sect = document.getElementById("inds");
@@ -81,24 +29,64 @@ d3.select("#inds").on("change", function() {
       //console.log(d);
       return d;
     }
-    if (d.shotType == "3PT Field Goal") {
-      console.log("3pt");
-    }
   });
-  renderPlot(data);
+  renderShots(data);
 });
 
+function addShots() {
+  // Shots
+  d3.csv("nba_savant.csv", function(d) {
+    return {
+      name: d.name,
+      teamName: d.team_name,
+      shotMadeFlag: d.shot_made_flag,
+      actionType: d.action_type,
+      shotType: d.shot_type,
+      x: d.x,
+      y: d.y
+    };
+  }).then(function(d) {
+    full_data = d;
+    renderShots(full_data);
+  });
+}
+
+function renderShots(data) {
+  var shots = shot_g.selectAll("circle").data(data);
+
+  shots
+    .enter()
+    .append("circle")
+    .merge(shots)
+    .attr("cx", d => shot_xScale(d.x))
+    .attr("cy", d => shot_yScale(d.y))
+    .attr("r", 5)
+    .attr("fill", d => (d.shotMadeFlag == 1 ? "red" : "blue"));
+
+  shots.exit().remove();
+}
+
 function draw_court() {
-  const width = WIDTH;
-  const height = (width / 50) * 47;
+  var chartDiv = document.getElementById("court");
+
+  var court = d3
+    .select(chartDiv)
+    .append("court")
+    .append("svg");
+  court.attr("width", width).attr("height", height);
+
+  var court_g = court.append("g");
   court_g.attr("width", width).attr("height", height);
 
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
-
+  const court_xScale = d3.scaleLinear().domain([-25, 25]);
+  const court_yScale = d3.scaleLinear().domain([-4, 43]);
   court_xScale.range([margin.left, innerWidth]).nice();
-
   court_yScale.range([margin.top, innerHeight]).nice();
+
+  var Basket = court_g.append("circle");
+  var CornerThreeLeft = court_g.append("rect");
+  var CornerThreeRight = court_g.append("rect");
+  var ThreeLine = court_g.append("path");
 
   Basket.attr("cx", court_xScale(0))
     .attr("cy", court_yScale(-0.75))
@@ -135,6 +123,8 @@ function draw_court() {
       "transform",
       "translate(" + court_xScale(0) + ", " + court_yScale(0) + ")"
     );
+
+  return court;
 }
 
 function appendArcPath(base, radius, startAngle, endAngle) {
